6577ff19b6f0cb2308ca63933123a19c2b33261e
Domenico Merola <misterdomix@hotmail.it>
2025-11-11 15:10:25 +0100

    Test
    Testing

---
+++ Implementing_Camera_LiveF/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Implementing_Camera_LiveF
+//
+//  Created by Domenico Merola on 10/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Implementing_Camera_LiveF/Implementing_Camera_LiveFApp.swift
@@ -0,0 +1,17 @@
+//
+//  Implementing_Camera_LiveFApp.swift
+//  Implementing_Camera_LiveF
+//
+//  Created by Domenico Merola on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Implementing_Camera_LiveFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
